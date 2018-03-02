06d9451cf567f59d30c9e61092167e2d3dccfe26
Deep Patel <deeppatel234@gmail.com>
2018-03-03 00:46:21 +0530

    add json data

--- js/home.js
+++ js/home.js
@@ -1,10 +1,16 @@
+
 $(document).ready(function () {
 
   $('#sidebarCollapse').on('click', function () {
     $('#sidebar').toggleClass('active');
   })
 
-  // particlesJS.load('sidebar', 'particles.json', function () {
-  //   console.log('callback - particles.js config loaded');
-  // });
+  $.getJSON('data.json', function (data) {
+    $('.home-page .title .firstname').text(data.firstName);
+    $('.home-page .title .lastname').text(data.lastName);
+    $('.social-icons.facebook').attr('href', data.facebook);
+    $('.social-icons.github').attr('href', data.github);
+    $('.social-icons.linkedin').attr('href', data.linkedin);
+  });
+
 })
