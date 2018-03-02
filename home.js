9da6cf086f1aa3248d17c7bec277d7214c704e69
Deep Patel <deeppatel234@gmail.com>
2018-03-02 22:47:18 +0530

    asds

---
+++ home.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+  $('#sidebarCollapse').on('click', function () {
+    $('#sidebar').toggleClass('active');
+  })
+
+  // particlesJS.load('sidebar', 'particles.json', function () {
+  //   console.log('callback - particles.js config loaded');
+  // });
+})
